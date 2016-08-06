40eeaa1ccde5114d586cb2d326f05a77307e5a38
Arthur Xavier <arthur.xavierx@gmail.com>
2016-08-06 01:11:57 -0300

    Fix 'Todo.focus' to return a real effect function

--- src/Todo/Todo.js
+++ src/Todo/Todo.js
@@ -1,8 +1,10 @@
 // module Todo.Todo
 
 exports.focusTodo = function(id) {
-  var $el = document.getElementById('todo-' + id);
-  if (document.activeNode !== $el) {
-    setTimeout($el.focus.bind($el), 100);
-  }
+  return function() {
+    var $el = document.getElementById('todo-' + id);
+    if (document.activeNode !== $el) {
+      $el.focus();
+    }
+  };
 };
